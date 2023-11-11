63474150b44f0edecf9ec87ba938c257475e557e
Machillka <2437844684@qq.com>
2023-11-11 18:55:26 +0800

    update

---
+++ C/TestProject/MVector.cpp
@@ -0,0 +1,23 @@
+#include "MVector.h"
+
+template<typename T >
+void MVector<T>::AddItem(T value)
+{
+	
+}
+
+template<typename T >
+MVector<T> MVector<T>::Dot(MVector<T> dotVec)
+{
+	// sizeA not equal to sizeB
+	if (this->shape != dotVec.shape)
+	{
+		//!TODO use exception 
+		printf("Shape Error")
+		return null;
+	}
+
+	for()
+
+	return this;
+}
